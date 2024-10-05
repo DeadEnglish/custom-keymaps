55623f8349b5b7bca790f9435a4c911bc0224433
Liam Canetti <git@liamcanetti.co.uk>
2024-10-05 20:04:32 +0100

    feat(corne): finally add keymap

--- keyboards/crkbd/rev1/keymaps/deadenglish/config.h
+++ keyboards/crkbd/rev1/keymaps/deadenglish/config.h
@@ -20,7 +20,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 #pragma once
 
-//#define USE_MATRIX_I2C
+#define USE_MATRIX_I2C
 
 /* Select hand configuration */
 
@@ -32,18 +32,22 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 //#define TAPPING_TERM 100
 
 #ifdef RGBLIGHT_ENABLE
-    #define RGBLIGHT_EFFECT_BREATHING
-    #define RGBLIGHT_EFFECT_RAINBOW_MOOD
-    #define RGBLIGHT_EFFECT_RAINBOW_SWIRL
-    #define RGBLIGHT_EFFECT_SNAKE
-    #define RGBLIGHT_EFFECT_KNIGHT
-    #define RGBLIGHT_EFFECT_CHRISTMAS
-    #define RGBLIGHT_EFFECT_STATIC_GRADIENT
-    #define RGBLIGHT_EFFECT_RGB_TEST
-    #define RGBLIGHT_EFFECT_ALTERNATING
-    #define RGBLIGHT_EFFECT_TWINKLE
+    #undef  RGBLIGHT_EFFECT_BREATHING
+    #undef  RGBLIGHT_EFFECT_RAINBOW_MOOD
+    #undef  RGBLIGHT_EFFECT_RAINBOW_SWIRL
+    #undef  RGBLIGHT_EFFECT_SNAKE
+    #undef  RGBLIGHT_EFFECT_KNIGHT
+    #undef  RGBLIGHT_EFFECT_CHRISTMAS
+    #undef  RGBLIGHT_EFFECT_STATIC_GRADIENT
+    #undef  RGBLIGHT_EFFECT_RGB_TEST
+    #undef  RGBLIGHT_EFFECT_ALTERNATING
+    #undef  RGBLIGHT_EFFECT_TWINKLE
+    #define RGBLIGHT_DEFAULT_MODE RGBLIGHT_MODE_STATIC_LIGHT
     #define RGBLIGHT_LIMIT_VAL 120
     #define RGBLIGHT_HUE_STEP 10
     #define RGBLIGHT_SAT_STEP 17
     #define RGBLIGHT_VAL_STEP 17
+    #define RGBLIGHT_DEFAULT_HUE 0
+    #define RGBLIGHT_DEFAULT_SAT 0
+    #define RGBLIGHT_DEFAULT_VAL 255
 #endif
